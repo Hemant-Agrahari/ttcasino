c7cb604b369b6241ae76e58218b3a3bb70b67913
Hemant Agarahari <hemantkagrahari@gmail.com>
2025-05-11 19:22:24 +0530

    set responsive

--- src/layout/Providers.tsx
+++ src/layout/Providers.tsx
@@ -4,20 +4,21 @@ import React from 'react';
 const Providers = () => {
   const cards = [
     { id: 1, src: '/assets/images/mm.webp', alt: 'Jackpot Race' },
-    { id: 2, src: '/assets/images/mm.webp', alt: 'Habanero' },
-    { id: 3, src: '/assets/images/mm.webp', alt: 'Greentube' },
-    { id: 4, src: '/assets/images/mm.webp', alt: 'Pragmatic Play' },
-    { id: 5, src: '/assets/images/mm.webp', alt: 'Smartsoft Gaming' },
-    { id: 6, src: '/assets/images/mm.webp', alt: 'AGT Software' },
-    { id: 7, src: '/assets/images/mm.webp', alt: 'Pragmatic Live' },
-     { id: 1, src: '/assets/images/mm.webp', alt: 'Jackpot Race' },
-    { id: 2, src: '/assets/images/mm.webp', alt: 'Habanero' },
-    { id: 3, src: '/assets/images/mm.webp', alt: 'Greentube' },
-    { id: 4, src: '/assets/images/mm.webp', alt: 'Pragmatic Play' },
-    { id: 5, src: '/assets/images/mm.webp', alt: 'Smartsoft Gaming' },
-    { id: 6, src: '/assets/images/mm.webp', alt: 'AGT Software' },
-    { id: 7, src: '/assets/images/mm.webp', alt: 'Pragmatic Live' },
+    { id: 2, src: '/assets/images/Endorphina.webp', alt: 'Habanero' },
+    { id: 3, src: '/assets/images/Habanero.webp', alt: 'Greentube' },
+    { id: 4, src: '/assets/images/Hacksaw.webp', alt: 'Pragmatic Play' },
+    { id: 5, src: '/assets/images/3_Oaks.webp', alt: 'Smartsoft Gaming' },
+    { id: 6, src: '/assets/images/bet-slot.webp', alt: 'AGT Software' },
+    { id: 8, src: '/assets/images/Pragmaticnew.webp', alt: 'Pragmatic Live' },
+    { id: 9, src: '/assets/images/mm.webp', alt: 'Jackpot Race' },
+    { id: 10, src: '/assets/images/Hacksaw.webp', alt: 'Habanero' },
+    { id: 11, src: '/assets/images/mm.webp', alt: 'Greentube' },
+    { id: 12, src: '/assets/images/Habanero.webp', alt: 'Pragmatic Play' },
+    { id: 13, src: '/assets/images/mm.webp', alt: 'Smartsoft Gaming' },
+    { id: 14, src: '/assets/images/Habanero.webp', alt: 'AGT Software' },
+    { id: 15, src: '/assets/images/Endorphina.webp', alt: 'Pragmatic Live' },
   ];
+
   return (
     <div className="provider-scroll-container">
       {cards.map((card) => (
