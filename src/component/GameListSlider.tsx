a44e605b610ba30a41c393255b1e6bafce3a3d38
Hemant Agarahari <hemantkagrahari@gmail.com>
2025-05-11 14:29:12 +0530

    Added gamesliderlist

--- src/component/GameListSlider.tsx
+++ src/component/GameListSlider.tsx
@@ -1,47 +1,95 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const GameListSlider = () => {
-   const categories = [
-    { name: 'All Games', icon: '/assets/images/home-1.svg' },
+  const categories = [
     {
-      name: 'Live Tables',
-      icon: '/assets/images/livetables.svg',
-      active: true,
+      type: 'text',
+      icon: '/assets/images/home-1.svg',
+      label: 'Home',
     },
-    { name: 'New', icon: '/assets/images/new.svg' },
-    { name: 'Bet Games', icon: '/assets/images/betgames.svg' },
-    { name: 'First Games', icon: '/assets/images/1stp.svg' },
-    { name: 'Blacl Jack', icon: '/assets/images/blackjackicon.svg' },
-    { name: 'New', icon: '/assets/images/new.svg' },
-    { name: 'Bet Games', icon: '/assets/images/betgames.svg' },
-    { name: 'First Games', icon: '/assets/images/1stp.svg' },
-    { name: 'Blacl Jack', icon: '/assets/images/blackjackicon.svg' },
-    { name: 'New', icon: '/assets/images/new.svg' },
-    { name: 'Bet Games', icon: '/assets/images/betgames.svg' },
-    { name: 'First Games', icon: '/assets/images/1stp.svg' },
-    { name: 'Blacl Jack', icon: '/assets/images/blackjackicon.svg' },
-    { name: 'New', icon: '/assets/images/new.svg' },
-    { name: 'Bet Games', icon: '/assets/images/betgames.svg' },
-    { name: 'First Games', icon: '/assets/images/1stp.svg' },
-    { name: 'Blacl Jack', icon: '/assets/images/blackjackicon.svg' },
-    { name: 'New', icon: '/assets/images/new.svg' },
-    { name: 'Bet Games', icon: '/assets/images/betgames.svg' },
-    { name: 'First Games', icon: '/assets/images/1stp.svg' },
-    { name: 'Blacl Jack', icon: '/assets/images/blackjackicon.svg' },
+    {
+      type: 'image',
+      image: '/assets/images/spinwin.webp',
+    },
+    {
+      type: 'image',
+      image: '/assets/images/34free.webp',
+    },
+    {
+      type: 'image',
+      image: '/assets/images/drops-wins.svg',
+    },
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
+    { type: 'text', label: 'Bet Games', icon: '/assets/images/betgames.svg' },
+    { type: 'text', label: 'First Games', icon: '/assets/images/1stp.svg' },
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
+    {
+      type: 'text',
+      icon: '/assets/images/aviator-anim.gif',
+      label: 'Crash',
+    },
+    { type: 'text', label: 'New', icon: '/assets/images/new.svg' },
+
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
+    {
+      type: 'text',
+      icon: '/assets/images/aviator-anim.gif',
+      label: 'Crash',
+    },
+    { type: 'text', label: 'New', icon: '/assets/images/new.svg' },
+
+    {
+      type: 'text',
+      icon: '/assets/images/blackjackicon.svg',
+      label: 'Live Tables',
+    },
   ];
   return (
-      <div className="game-list-scroll-container">
-          {categories.map((item, index) => (
-            <div
-              key={index}
-              className={`scroll-item ${item.active ? 'active' : ''}`}
-            >
-              <img src={item.icon} alt={item.name} className="icon" />
-              <span className="label">{item.name}</span>
-            </div>
-          ))}
+    <div className="game-list-slider-wrapper">
+      {categories.map((cat, index) => (
+        <div key={index} className="game-list-item">
+          {cat.type === 'text' ? (
+            <>
+              <div className="icon-wrapper">
+                <Image src={cat.icon} alt={cat.label} width={30} height={30} />
+              </div>
+              <span className="label">{cat.label}</span>
+            </>
+          ) : (
+            <Image
+              src={cat.image}
+              alt="Category Image"
+              width={60}
+              height={60}
+            />
+          )}
         </div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default GameListSlider
+export default GameListSlider;
